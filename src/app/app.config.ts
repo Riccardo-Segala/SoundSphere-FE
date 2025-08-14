c97d4a229bd17ad45d10f7f8cc506f2f0151acac
Jacopo Petrini <jacopo.petrini@edu.unife.it>
2025-08-14 23:03:34 +0200

    Creazione primi component (login,registrazione,catalogo),e primi switch di pagina.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import { routes } from './app.routes';
+import {ApiModule} from '';
 
 export const appConfig: ApplicationConfig = {
   providers: [
---
+++ src/app/services/auth-service.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+    private authUrl="http://localhost:8080/api/utente";
+    constructor(private http: HttpClient) {
+    }
+    //possibilita che invece di /${email} sia ?email=${email}
+    getUserByEmail(email:string,password:string):Observable<any> {
+        return this.http.get(`${this.authUrl}/${email}`);
+    }
+}
